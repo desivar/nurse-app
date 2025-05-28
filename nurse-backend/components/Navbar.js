fb6b96b80d6496a5a61e92646c8603a09be50e67
desivar <jillian_desire@yahoo.es>
2025-05-27 20:37:38 -0600

    updated Navbar

--- nurse-backend/components/Navbar.js
+++ nurse-backend/components/Navbar.js
@@ -4,8 +4,24 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/patients">Patients</Link>
+      <h1>Nurse App</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/patients">Patients</Link>
+        </li>
+        <li>
+          <Link to="/medications">Medications</Link>
+        </li>
+        <li>
+          <Link to="/health-records">Health Records</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
